81fcda40eca22d33b4dc78f5de7d213e0440e2ad
artepatrick <45848477+artepatrick@users.noreply.github.com>
2023-05-16 20:59:24 -0300

    Adicionando função para o celular

--- script.js
+++ script.js
@@ -7,3 +7,22 @@ function restoreImage() {
     const image = document.getElementById('imagemDupla');
     image.src = 'https://i.postimg.cc/QCgFRcFj/papelzinho-no-ch-o-02.jpg';
 }
+
+/* function clickImageLegacy() {
+    console.log('clickImage');
+    const image = document.getElementById('imagemDupla');
+    console.log(image.src);
+    if (image.src === 'https://i.postimg.cc/QCgFRcFj/papelzinho-no-ch-o-01.jpg') {
+        console.log('a imagem é a 2');
+    }
+} */
+
+function clickImage() {
+    console.log('clickImage!!!');
+    const image = document.getElementById('imagemDupla');
+    if (image.src.includes('papelzinho-no-ch-o-01.jpg')) {
+        image.src = 'https://i.postimg.cc/QCgFRcFj/papelzinho-no-ch-o-02.jpg';
+    } else {
+        image.src = 'https://i.postimg.cc/QCgFRcFj/papelzinho-no-ch-o-01.jpg';
+    }
+}
