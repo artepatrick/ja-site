d9f1e29699c8254f3c253f23e537a26c588465ee
artepatrick <45848477+artepatrick@users.noreply.github.com>
2023-05-16 20:42:49 -0300

    inserindo javascript com foto que troca no hover

---
+++ script.js
@@ -0,0 +1,9 @@
+function changeImage() {
+    const image = document.getElementById('imagemDupla');
+    image.src = 'https://i.postimg.cc/3xtv8jnR/papelzinho-no-ch-o-01.jpg';
+}
+
+function restoreImage() {
+    const image = document.getElementById('imagemDupla');
+    image.src = 'https://i.postimg.cc/QCgFRcFj/papelzinho-no-ch-o-02.jpg';
+}
