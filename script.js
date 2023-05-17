713d7bac961ea90e03ff4af0f834618fca04ad0b
artepatrick <45848477+artepatrick@users.noreply.github.com>
2023-05-16 21:10:16 -0300

    adaptando para o celular

--- script.js
+++ script.js
@@ -8,21 +8,17 @@ function restoreImage() {
     image.src = 'https://i.postimg.cc/QCgFRcFj/papelzinho-no-ch-o-02.jpg';
 }
 
-/* function clickImageLegacy() {
-    console.log('clickImage');
+function changeImageOnTap() {
     const image = document.getElementById('imagemDupla');
-    console.log(image.src);
-    if (image.src === 'https://i.postimg.cc/QCgFRcFj/papelzinho-no-ch-o-01.jpg') {
-        console.log('a imagem é a 2');
-    }
-} */
 
-function clickImage() {
-    console.log('clickImage!!!');
-    const image = document.getElementById('imagemDupla');
+    // Check the current source and update it accordingly
     if (image.src.includes('papelzinho-no-ch-o-01.jpg')) {
-        image.src = 'https://i.postimg.cc/QCgFRcFj/papelzinho-no-ch-o-02.jpg';
+        image.src = 'https://i.postimg.cc/3xtv8jnR/papelzinho-no-ch-o-02.jpg';
     } else {
         image.src = 'https://i.postimg.cc/QCgFRcFj/papelzinho-no-ch-o-01.jpg';
     }
 }
+
+// Add event listener for the touchstart event
+const image = document.getElementById('imagemDupla');
+image.addEventListener('touchstart', changeImageOnTap);
